3ff0965df75d1821dcf8026617ce2122820529b9
Brad Sickles <brad.sickles@gmail.com>
2024-03-22 09:16:02 -0400

    Add additional outputs to aid attachment.

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,25 @@ output "filestore_id" {
 
 output "filestore_name" {
   value       = google_filestore_instance.this.name
-  description = "string ||| The name of the Filestore instance"
+  description = "string ||| The name of the Filestore instance."
+}
+
+output "filestore_location" {
+  value = google_filestore_instance.this.location
+  description = "string ||| The GCP location of the Filestore instance."
+}
+
+output "filestore_capacity" {
+  value       = var.capacity
+  description = "number ||| The capacity of the Filestore in GiB."
+}
+
+output "filestore_ips" {
+  value       = google_filestore_instance.this.networks[0].ip_addresses
+  description = "string ||| The IP addresses of the Filestore."
+}
+
+output "filestore_share_name" {
+  value       = google_filestore_instance.this.file_shares[0].name
+  description = "string ||| The name of the sole file share created on the Filestore."
 }
